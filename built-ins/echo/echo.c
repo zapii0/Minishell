a156884a1c4bbe336de298fed6fe6b6cf60fa7f4
Albert Pieniak <mojo.pl.006@gmail.com>
2025-08-15 20:12:55 +0200

    echo and struct added

--- built-ins/echo/echo.c
+++ built-ins/echo/echo.c
@@ -6,7 +6,52 @@
 /*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 14:23:17 by apieniak          #+#    #+#             */
-/*   Updated: 2025/08/14 14:23:18 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/08/15 19:31:53 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/minishell.h"
+
+int	input_check(int argc)
+{
+	if (argc <= 1)
+	{
+		printf("\n");
+		return (1);
+	}
+	return (0);
+}
+void parameter_check(int argc, char **argv)
+{
+	int i;
+
+	i = 2;
+	while (i < argc)
+	{
+		printf("%s", argv[i]);
+		if ((i + 1) != argc)
+			printf(" ");
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	if (input_check(argc))
+		exit(0);
+	if (strcmp(argv[1], "-n") == 0)
+		parameter_check(argc, argv);
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			printf("%s ", argv[i]);
+			i++;
+		}
+		printf("\n");
+	}
+	return (0);
+}
--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:04:48 by apieniak          #+#    #+#             */
-/*   Updated: 2025/08/14 15:21:22 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/08/15 19:56:29 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,17 @@ typedef struct s_env
 	char *value;
 	struct s_env *next;
 } t_env;
+
+typedef struct s_pipes
+{
+	int pfd[2];
+	int old_pfd[2];
+} t_pipes;
+
+typedef struct s_data
+{
+	
+} t_data;
 
 // The function where sigaction is structed, and started
 void	gather_signal(void);
