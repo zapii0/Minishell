a743129476d95e40cd4b219a1f1764c31c88ce81
Albert Pieniak <mojo.pl.006@gmail.com>
2025-11-20 16:27:02 +0100

    done

--- execution/execution_utils.c
+++ execution/execution_utils.c
@@ -6,78 +6,118 @@
 /*   By: apieniak <apieniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 21:27:58 by apieniak          #+#    #+#             */
-/*   Updated: 2025/11/20 14:33:45 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:24:09 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static int	open_input_files(char **red_in)
+{
+	int	fd;
+	int	i;
+	int	last_fd;
+
+	i = 0;
+	last_fd = -1;
+	while (red_in[i])
+	{
+		fd = open(red_in[i], O_RDONLY);
+		if (fd == -1)
+			return (perror(red_in[i]), -1);
+		if (last_fd != -1)
+			close(last_fd);
+		last_fd = fd;
+		i++;
+	}
+	return (last_fd);
+}
+
+static int	handle_both_inputs(t_data *d, t_pipes *pipes)
+{
+	int	fd;
+
+	if (d->b_heredoc)
+	{
+		dup2(pipes->heredoc[0], STDIN_FILENO);
+		close(pipes->heredoc[0]);
+		return (0);
+	}
+	fd = open_input_files(d->red_in);
+	if (fd == -1)
+		return (1);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
 int	handle_input_redir(t_data *d, t_pipes *pipes)
 {
 	int	fd;
 
+	if (d->heredoc && d->red_in && d->red_in[0])
+		return (handle_both_inputs(d, pipes));
+	if (d->red_in && d->red_in[0])
+	{
+		fd = open_input_files(d->red_in);
+		if (fd == -1)
+			return (1);
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		return (0);
+	}
 	if (d->heredoc)
 	{
 		dup2(pipes->heredoc[0], STDIN_FILENO);
 		close(pipes->heredoc[0]);
 		return (0);
 	}
-	if (d->red_in && d->red_in[0])
-	{
-		fd = open(d->red_in[0], O_RDONLY);
-		if (fd == -1)
-		{
-			perror(d->red_in[0]);
-			return (1);
-		}
-		dup2(fd, STDIN_FILENO);
-		return (close(fd), 0);
-	}
 	if (pipes->old_pfd > 0)
-	{
-		dup2(pipes->old_pfd, STDIN_FILENO);
-		return (close(pipes->old_pfd), 0);
-	}
+		return (dup2(pipes->old_pfd, STDIN_FILENO), close(pipes->old_pfd), 0);
 	return (0);
+}
+
+static int	open_output_files(char **red_out, bool append)
+{
+	int	fd;
+	int	flags;
+	int	i;
+	int	last_fd;
+
+	flags = O_WRONLY | O_CREAT;
+	if (append)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	i = 0;
+	last_fd = -1;
+	while (red_out[i])
+	{
+		fd = open(red_out[i], flags, 0644);
+		if (fd == -1)
+			return (perror(red_out[i]), -1);
+		if (last_fd != -1)
+			close(last_fd);
+		last_fd = fd;
+		i++;
+	}
+	return (last_fd);
 }
 
 int	handle_output_redir(t_data *d, t_pipes *pipes, t_base *base)
 {
 	int	fd;
-	int	flags;
 
 	if (d->red_out && d->red_out[0])
 	{
-		flags = O_WRONLY | O_CREAT;
-		if (d->append)
-			flags |= O_APPEND;
-		else
-			flags |= O_TRUNC;
-		fd = open(d->red_out[0], flags, 0644);
+		fd = open_output_files(d->red_out, d->append);
 		if (fd == -1)
-		{
-			perror(d->red_out[0]);
 			return (1);
-		}
 		dup2(fd, STDOUT_FILENO);
-		return (close(fd), 0);
+		close(fd);
+		return (0);
 	}
 	if (base->index < base->pipes_num)
-	{
-		dup2(pipes->pfd[1], STDOUT_FILENO);
-		return (close(pipes->pfd[1]), 0);
-	}
+		return (dup2(pipes->pfd[1], STDOUT_FILENO), close(pipes->pfd[1]), 0);
 	return (0);
-}
-
-void	close_pipe_fds(t_pipes *pipes)
-{
-	if (pipes->pfd[0] > 0)
-		close(pipes->pfd[0]);
-	if (pipes->pfd[1] > 0)
-		close(pipes->pfd[1]);
-	if (pipes->old_pfd > 0)
-		close(pipes->old_pfd);
-	if (pipes->heredoc[0] > 0)
-		close(pipes->heredoc[0]);
 }
--- execution/list_to_array.c
+++ execution/list_to_array.c
@@ -6,7 +6,7 @@
 /*   By: apieniak <apieniak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 18:35:59 by apieniak          #+#    #+#             */
-/*   Updated: 2025/11/20 15:16:39 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:25:13 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,18 @@ void	ft_print_error(char *cmd, char *path, char *mess, int exit_status)
 	if (path)
 		free(path);
 	exit(exit_status);
+}
+
+void	close_pipe_fds(t_pipes *pipes)
+{
+	if (pipes->pfd[0] > 0)
+		close(pipes->pfd[0]);
+	if (pipes->pfd[1] > 0)
+		close(pipes->pfd[1]);
+	if (pipes->old_pfd > 0)
+		close(pipes->old_pfd);
+	if (pipes->heredoc[0] > 0)
+		close(pipes->heredoc[0]);
 }
 
 void	print_env_arr(t_base *base)
